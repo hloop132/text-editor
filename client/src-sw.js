7c40b0a484a19603f6f5e5e0946cb1f036049ab6
heather loop <hloop132@gmail.com>
2022-02-28 17:36:49 -0800

    jate

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { request } = require('express');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -21,16 +22,16 @@ const pageCache = new CacheFirst({
 
 warmStrategyCache({
   urls: ['/index.html', '/'],
-  strategy: pageCache,
+  strategy: pageCache
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-// Register route for caching images
-
-registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+registerRoute(({ request }) =>
+  request.destination === 'style' ||
+  request.destination === 'script'||
+  request.destination === 'worker',
   new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,63 +1,40 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-// We are creating a new database named 'jate' which will be using version 1 of the database.
-  openDB('jate', 1, {
+  // We are creating a new database named 'jate' which will be using version 1 of the database.
+  openDB("jate", 1, {
     // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-    // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
-    db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created 🖖');
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created 🖖");
     },
   });
 
-// Export a function we will use to PUT to the database.
 // TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error('putDb not implemented');
-
-// Export a function use to POST to the database.
-export const putDb = async (content)  => {
-  console.log('Update the database, what a giver');
-  // Create a connection to the database 
-  const jateDb = await openDB('jate', 1);
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
-  // Open up the object store.
-  const store = tx.objectStore('jate');
-  
-  const request = store.put({ id: 1, value: content });
-  // Get confirmation of the request.
+export const putDb = async (content) => {
+  console.log('PUT to the jate database');
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, text:content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result.vale);
-
+  console.log("🚀 - data saved to the database", result);
 };
-
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.log('GET from the database, so greedy');
-  // Create a connection to the database 
-  const jateDb = await openDB('jate', 1);
-  // Create a new transaction 
-  const tx = jateDb.transaction('jate', 'readonly');
-  
-  const store = tx.objectStore('jate');
-  
+export const getDb = async (e) => {
+  console.log('PUT to the jate database');
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const request = store.get(1);
-
   const result = await request;
-  console.log('result.value', result.value);
-  if(result.value != undefined){
-    return result.value;
-  } else {
-    console.log("dang, sorry man");
-    return;
-  }
- 
+  console.log("result.value", result);
+  return result;
 };
-
 
 initdb();
